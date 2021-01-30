f8b26eba7804e7c4ffb196dc24224208e57a7116
Duncalicious <dbru997@gmail.com>
2021-01-30 09:42:49 -0700

    First version of CodeGuessResponse working

--- lib/main.rb
+++ lib/main.rb
@@ -2,32 +2,39 @@
 module CodeGuessResponse
   def guess_check(guess)
     temp_arr = []
+    temp_guess = []
     clues = []
     self.code.each_with_index do |e, i|
       if e == guess[i]
         clues.push('x')
       else
         temp_arr.push(e)
+        temp_guess.push(guess[i])
       end
     end
 
-    guess.each do |e|
-      if uniq_temp_arr.include?(e)
-        num = guess.count(e) < temp_arr.count(e) ? guess.count(e) : temp_arr.count(e)
-        p "e:#{e} num:#{num}"
+    digits = [1,2,3,4,5,6]
+
+    digits.each do |dij|
+      if temp_arr.count(dij) <= temp_guess.count(dij)
+        temp_arr.count(dij).times do
+          clues.push("o")
+        end
+      else
+        temp_guess.count(dij).times do
+          clues.push("o")
+        end
       end
     end
-
-    p guess
-    p self.code
-    p clues
-    "x x x x"
+  #  p guess
+  #  p self.code
+  #  p clues
+    clues.join(" ")
   end
 end
 
 class CpuCodeMaker
   include CodeGuessResponse
-
   attr_reader :code
 
   def initialize
@@ -42,4 +49,4 @@ class CpuCodeMaker
   end
 end
 
-CpuCodeMaker.new.guess_check([2,2,5,5])
+#p CpuCodeMaker.new.guess_check([1,1,1,2])
